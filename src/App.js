57f70f07db898ee56b578a2f744fc4c02a52ff23
winar-jin <winar1994@outlook.com>
2018-03-02 21:41:42 +0800

    [jin] modify easydss file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React, {Component} from 'react';
 import './App.css';
-import Demo from './component/Demo';
-import {
-  BrowserRouter as Router,
-  Route,
-  Redirect,
-  Link
-} from 'react-router-dom';
+import EasyDSS from './component/EasyDSS';
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Redirect,
+//   Link
+// } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Demo />
-        </div>
-      </Router>
+      <div className="App">
+        <EasyDSS/>
+      </div>
     );
   }
 }
