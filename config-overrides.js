021ad273adfbc23120a58131df8c1637ecf20523
winar-jin <winar1994@outlook.com>
2018-03-02 10:53:17 +0800

    [jin] change the primary color of theme

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,11 @@
 const { injectBabelPlugin } = require('react-app-rewired');
+const rewireLess = require('react-app-rewire-less');
 
 module.exports = function override(config, env) {
-  config = injectBabelPlugin(['import', { libraryName: 'antd', style: 'css' }], config);
+  // config = injectBabelPlugin(['import', { libraryName: 'antd', style: 'css' }], config);
+  config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);  // change importing css to less
+  config = rewireLess.withLoaderOptions({
+    modifyVars: { "@primary-color": "#18A55C" },
+  })(config, env);  
   return config;
 };
--- src/component/Demo.js
+++ src/component/Demo.js
@@ -10,9 +10,10 @@ class Demo extends Component {
         {/*</header>*/}
         {/*<p className="App-intro">*/}
           {/*To get started, edit <code>src/App.js</code> and save to reload.*/}
-          {/*<Button type="primary">Button</Button>*/}
+          <Button type="primary">Button</Button>
         {/*</p>*/}
         {/*<iframe allowFullScreen="true" width="100%" height="1000px" frameborder="0" scrolling="no" src="http://10.211.55.6:10080/admin/main.html"></iframe>*/}
+        
         <iframe width='560' height='315' src='http://10.211.55.6:10080/api/play/b2779a90017e11e8895a734ee1bad023' frameborder='0' allowfullscreen></iframe>
       </div>
     );
