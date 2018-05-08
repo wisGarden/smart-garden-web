cc98695961cc48bc2d8485788e8c6a1b9d71c78a
winar-jin <winar1994@outlook.com>
2018-05-08 14:57:17 +0800

    [exper] hide system setting temporary

--- src/component/TrafficSetting.js
+++ src/component/TrafficSetting.js
@@ -82,97 +82,97 @@ class TrafficSetting extends Component {
             <WrappedAuthNewForm/>
           </TabPane>
         ) : null}
-        <TabPane tab="服务配置" key="systemSetting">
-          <iframe
-            style={{}}
-            title="setting"
-            allowFullScreen="true"
-            width="100%"
-            height="600px"
-            scrolling="no"
-            src={`http://${config.vodServerUrl}/admin/panel.html?page=ums_config.html`}
-            frameBorder="0"
-          />
-          {/*<Collapse bordered={false}>*/}
-          {/*<Panel header="录像上传路径" key="upload_dir">*/}
-          {/*<Row>*/}
-          {/*<Col span={4} offset={1}>*/}
-          {/*<Input*/}
-          {/*disabled={this.state.is_edit_upload_dir}*/}
-          {/*value={this.state.upload_dir}*/}
-          {/*placeholder={'请输入文件上传路径'}*/}
-          {/*onChange={e => {*/}
-          {/*this.setState({*/}
-          {/*upload_dir: e.target.value*/}
-          {/*}, () => {*/}
-          {/*});*/}
-          {/*}}/>*/}
-          {/*</Col>*/}
-          {/*<Col span={1} style={{*/}
-          {/*lineHeight: '32px',*/}
-          {/*marginLeft: '15px'*/}
-          {/*}}>*/}
-          {/*{this.state.is_edit_upload_dir ?*/}
-          {/*<Tooltip title={'编辑'}>*/}
-          {/*<Icon onClick={_ => {*/}
-          {/*this.setState({*/}
-          {/*is_edit_upload_dir: false*/}
-          {/*});*/}
-          {/*}} className={'icon-hover'} type={'edit'}/>*/}
-          {/*</Tooltip> :*/}
-          {/*<Tooltip title={'完成'}>*/}
-          {/*<Icon onClick={_ => {*/}
-          {/*this.setState({*/}
-          {/*is_edit_upload_dir: true*/}
-          {/*}, () => {*/}
-          {/*this.updateSetting('upload_dir', '文件上传路径修改成功！');*/}
-          {/*});*/}
-          {/*}} className={'icon-hover'} type={'check'}/>*/}
-          {/*</Tooltip>*/}
-          {/*}*/}
-          {/*</Col>*/}
-          {/*</Row>*/}
-          {/*</Panel>*/}
-          {/*<Panel header="接口调用密钥" key="api_key">*/}
-          {/*<Row>*/}
-          {/*<Col span={4} offset={1}>*/}
-          {/*<Input*/}
-          {/*disabled={this.state.is_edit_api_key}*/}
-          {/*value={this.state.api_key}*/}
-          {/*placeholder={'请输入接口调用密钥'}*/}
-          {/*onChange={e => {*/}
-          {/*this.setState({*/}
-          {/*api_key: e.target.value*/}
-          {/*});*/}
-          {/*}}/>*/}
-          {/*</Col>*/}
-          {/*<Col span={1} style={{*/}
-          {/*lineHeight: '32px',*/}
-          {/*marginLeft: '15px'*/}
-          {/*}}>*/}
-          {/*{this.state.is_edit_api_key ?*/}
-          {/*<Tooltip title={'编辑'}>*/}
-          {/*<Icon onClick={_ => {*/}
-          {/*this.setState({*/}
-          {/*is_edit_api_key: false*/}
-          {/*});*/}
-          {/*}} className={'icon-hover'} type={'edit'}/>*/}
-          {/*</Tooltip> :*/}
-          {/*<Tooltip title={'完成'}>*/}
-          {/*<Icon onClick={_ => {*/}
-          {/*this.setState({*/}
-          {/*is_edit_api_key: true*/}
-          {/*}, () => {*/}
-          {/*this.updateSetting('api_key', '接口调用密钥修改成功！');*/}
-          {/*});*/}
-          {/*}} className={'icon-hover'} type={'check'}/>*/}
-          {/*</Tooltip>*/}
-          {/*}*/}
-          {/*</Col>*/}
-          {/*</Row>*/}
-          {/*</Panel>*/}
-          {/*</Collapse>*/}
-        </TabPane>
+        {/*<TabPane tab="服务配置" key="systemSetting">*/}
+          {/*<iframe*/}
+            {/*style={{}}*/}
+            {/*title="setting"*/}
+            {/*allowFullScreen="true"*/}
+            {/*width="100%"*/}
+            {/*height="600px"*/}
+            {/*scrolling="no"*/}
+            {/*src={`http://${config.vodServerUrl}/admin/panel.html?page=ums_config.html`}*/}
+            {/*frameBorder="0"*/}
+          {/*/>*/}
+          {/*/!*<Collapse bordered={false}>*!/*/}
+          {/*/!*<Panel header="录像上传路径" key="upload_dir">*!/*/}
+          {/*/!*<Row>*!/*/}
+          {/*/!*<Col span={4} offset={1}>*!/*/}
+          {/*/!*<Input*!/*/}
+          {/*/!*disabled={this.state.is_edit_upload_dir}*!/*/}
+          {/*/!*value={this.state.upload_dir}*!/*/}
+          {/*/!*placeholder={'请输入文件上传路径'}*!/*/}
+          {/*/!*onChange={e => {*!/*/}
+          {/*/!*this.setState({*!/*/}
+          {/*/!*upload_dir: e.target.value*!/*/}
+          {/*/!*}, () => {*!/*/}
+          {/*/!*});*!/*/}
+          {/*/!*}}/>*!/*/}
+          {/*/!*</Col>*!/*/}
+          {/*/!*<Col span={1} style={{*!/*/}
+          {/*/!*lineHeight: '32px',*!/*/}
+          {/*/!*marginLeft: '15px'*!/*/}
+          {/*/!*}}>*!/*/}
+          {/*/!*{this.state.is_edit_upload_dir ?*!/*/}
+          {/*/!*<Tooltip title={'编辑'}>*!/*/}
+          {/*/!*<Icon onClick={_ => {*!/*/}
+          {/*/!*this.setState({*!/*/}
+          {/*/!*is_edit_upload_dir: false*!/*/}
+          {/*/!*});*!/*/}
+          {/*/!*}} className={'icon-hover'} type={'edit'}/>*!/*/}
+          {/*/!*</Tooltip> :*!/*/}
+          {/*/!*<Tooltip title={'完成'}>*!/*/}
+          {/*/!*<Icon onClick={_ => {*!/*/}
+          {/*/!*this.setState({*!/*/}
+          {/*/!*is_edit_upload_dir: true*!/*/}
+          {/*/!*}, () => {*!/*/}
+          {/*/!*this.updateSetting('upload_dir', '文件上传路径修改成功！');*!/*/}
+          {/*/!*});*!/*/}
+          {/*/!*}} className={'icon-hover'} type={'check'}/>*!/*/}
+          {/*/!*</Tooltip>*!/*/}
+          {/*/!*}*!/*/}
+          {/*/!*</Col>*!/*/}
+          {/*/!*</Row>*!/*/}
+          {/*/!*</Panel>*!/*/}
+          {/*/!*<Panel header="接口调用密钥" key="api_key">*!/*/}
+          {/*/!*<Row>*!/*/}
+          {/*/!*<Col span={4} offset={1}>*!/*/}
+          {/*/!*<Input*!/*/}
+          {/*/!*disabled={this.state.is_edit_api_key}*!/*/}
+          {/*/!*value={this.state.api_key}*!/*/}
+          {/*/!*placeholder={'请输入接口调用密钥'}*!/*/}
+          {/*/!*onChange={e => {*!/*/}
+          {/*/!*this.setState({*!/*/}
+          {/*/!*api_key: e.target.value*!/*/}
+          {/*/!*});*!/*/}
+          {/*/!*}}/>*!/*/}
+          {/*/!*</Col>*!/*/}
+          {/*/!*<Col span={1} style={{*!/*/}
+          {/*/!*lineHeight: '32px',*!/*/}
+          {/*/!*marginLeft: '15px'*!/*/}
+          {/*/!*}}>*!/*/}
+          {/*/!*{this.state.is_edit_api_key ?*!/*/}
+          {/*/!*<Tooltip title={'编辑'}>*!/*/}
+          {/*/!*<Icon onClick={_ => {*!/*/}
+          {/*/!*this.setState({*!/*/}
+          {/*/!*is_edit_api_key: false*!/*/}
+          {/*/!*});*!/*/}
+          {/*/!*}} className={'icon-hover'} type={'edit'}/>*!/*/}
+          {/*/!*</Tooltip> :*!/*/}
+          {/*/!*<Tooltip title={'完成'}>*!/*/}
+          {/*/!*<Icon onClick={_ => {*!/*/}
+          {/*/!*this.setState({*!/*/}
+          {/*/!*is_edit_api_key: true*!/*/}
+          {/*/!*}, () => {*!/*/}
+          {/*/!*this.updateSetting('api_key', '接口调用密钥修改成功！');*!/*/}
+          {/*/!*});*!/*/}
+          {/*/!*}} className={'icon-hover'} type={'check'}/>*!/*/}
+          {/*/!*</Tooltip>*!/*/}
+          {/*/!*}*!/*/}
+          {/*/!*</Col>*!/*/}
+          {/*/!*</Row>*!/*/}
+          {/*/!*</Panel>*!/*/}
+          {/*/!*</Collapse>*!/*/}
+        {/*</TabPane>*/}
       </Tabs>
     );
   };
